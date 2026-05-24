099aab3184bb1219e9484ebbb10c3e2b774c5f4b
Phoebe <ruthowy@gmail.com>
2026-05-24 00:30:07 -1100

    I made changes to the add button as instructed in my assignment

--- scripts.js
+++ scripts.js
@@ -37,6 +37,25 @@ check.addEventListener("click", function () {
 });
 
 
+let score = 20;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".btn.again").addEventListener('click', function () {});
+  
+score = 20;
+secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+document.querySelector(".message").textContent = "Start guessing...";
+document.querySelector(".score").textContent = score;
+document.querySelector(".number").textContent = "?";
+document.querySelector(".guess").value = "";
+
+document.querySelector("body").style.backgroundColor = "#222";
+document.querySelector(".number").style.width = "15rem"; 
+
+
+  
+
+
 // reset implementeation
 
 
